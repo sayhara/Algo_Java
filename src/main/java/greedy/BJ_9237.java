4deae42a0cfcfeaa6017db389c83466ff08a67f4
sayhara <impact82@naver.com>
2024-01-05 23:05:41 +0900

    [그리디] 백준9237

---
+++ src/main/java/greedy/BJ_9237.java
@@ -0,0 +1,28 @@
+package greedy;
+import java.io.*;
+import java.util.*;
+
+public class BJ_9237 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        int N=Integer.parseInt(br.readLine());
+        Integer B_tree[]=new Integer[N];
+
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            B_tree[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(B_tree,Collections.reverseOrder());
+
+        Integer A_tree[]=new Integer[N];
+        for(int i=0;i<N;i++){
+            A_tree[i]=(i+1)+B_tree[i];
+        }
+        Arrays.sort(A_tree,Collections.reverseOrder());
+        System.out.println(A_tree[0]+1);
+
+    }
+}
