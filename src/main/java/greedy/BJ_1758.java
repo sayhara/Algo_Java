c3637b51750532b393b0ee79fae774ba32d0e2e3
sayhara <impact82@naver.com>
2024-01-06 21:33:30 +0900

    [그리디] 백준 1758

---
+++ src/main/java/greedy/BJ_1758.java
@@ -0,0 +1,26 @@
+package greedy;
+import java.io.*;
+import java.util.*;
+
+public class BJ_1758 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        int N=Integer.parseInt(br.readLine());
+        Integer arr[]=new Integer[N];
+        for(int i=0;i<N;i++){
+            arr[i]=Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+        long sum=0;
+        for(int i=0;i<N;i++){
+            int money=arr[i]-((i+1)-1);
+            if(money>0) sum+=money;
+        }
+        System.out.println(sum);
+
+    }
+}
