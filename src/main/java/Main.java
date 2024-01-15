918f6f5719adb2a06170bdea3a85655a2e3bfe2b
sayhara <impact82@naver.com>
2024-01-15 22:34:08 +0900

    [그리디] 1080, 2810 review

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+
+        int N=Integer.parseInt(br.readLine());
+        String s=br.readLine();
+        String result="";
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='S') result+="*S";
+            else {
+                result+="*LL";
+                i++;
+            }
+        }
+        result+="*";
+
+        int cnt=0;
+        for(int i=0;i<result.length();i++){
+            if(result.charAt(i)=='*') cnt++;
+        }
+
+        System.out.println(Math.min(cnt,N));
 
     }
 }
