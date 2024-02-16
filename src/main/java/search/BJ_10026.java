086cd5572505d79d9054fa7f16172f87c363052c
sayhara <impact82@naver.com>
2024-02-16 19:21:30 +0900

    [탐색] 1920, 1987, 2805, 10026, 10816

---
+++ src/main/java/search/BJ_10026.java
@@ -0,0 +1,76 @@
+package search;
+import java.io.*;
+import java.util.*;
+
+public class BJ_10026 {
+
+    static int N;
+    static char arr[][];
+    static boolean visited[][];
+    static int cnt1=0;
+    static int cnt2=0;
+    static int dr[]={-1,0,1,0};
+    static int dc[]={0,1,0,-1};
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        N=Integer.parseInt(br.readLine());
+        arr=new char[N][N];
+
+
+        for(int i=0;i<N;i++){
+            String s=br.readLine();
+            for(int j=0;j<s.length();j++){
+                arr[i][j]=s.charAt(j);
+            }
+        }
+
+        visited=new boolean[N][N];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(!visited[i][j]){
+                    DFS(i,j,arr[i][j]);
+                    cnt1++;
+                }
+            }
+        }
+
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(arr[i][j]=='R'){
+                    arr[i][j]='G';
+                }
+            }
+        }
+
+        visited=new boolean[N][N];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(!visited[i][j]){
+                    DFS(i,j,arr[i][j]);
+                    cnt2++;
+                }
+            }
+        }
+
+        System.out.println(cnt1+" "+cnt2);
+    }
+
+    public static void DFS(int r, int c, char val){
+
+        visited[r][c]=true;
+
+        for(int i=0;i<4;i++){
+            int nr=r+dr[i];
+            int nc=c+dc[i];
+
+            if(nr>=0 && nc>=0 && nr<N && nc<N){
+                if(arr[nr][nc]==val && !visited[nr][nc]){
+                    DFS(nr,nc,val);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/search/BJ_2805.java
@@ -0,0 +1,40 @@
+package search;
+import java.io.*;
+import java.util.*;
+
+public class BJ_2805 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        int N=Integer.parseInt(st.nextToken());
+        int M=Integer.parseInt(st.nextToken());
+        int tree[]=new int[N];
+
+        st=new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            tree[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(tree);
+
+        int left=0;
+        int right=tree[N-1];
+        int mid;
+
+        while(left<=right){
+            mid=(left+right)/2;
+            long sum=0;
+            for(int i=0;i<N;i++){
+                if(tree[i]>mid) sum+=tree[i]-mid;
+            }
+            if(sum<M){
+                right=mid-1;
+            } else if(sum>=M){
+                left=mid+1;
+            }
+        }
+        System.out.println(right);
+    }
+}
