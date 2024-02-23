45be69e13a479ceb6f32584359977f0989d83f27
sayhara <impact82@naver.com>
2024-02-23 23:28:06 +0900

    [탐색] 4673

---
+++ src/main/java/search/BJ_4673.java
@@ -0,0 +1,27 @@
+package search;
+
+public class BJ_4673 {
+
+    public static void main(String[] args) {
+
+        int sum,num;
+        boolean check[]=new boolean[10001];
+
+        for(int i=1;i<=10000;i++){
+            sum=0;
+            num=i;
+            sum+=num;
+            while(num!=0){
+                sum+=num%10;
+                num/=10;
+            }
+            if(sum<=10000) check[sum]=true;
+        }
+
+        for(int i=1;i<=10000;i++){
+            if(!check[i]){
+                System.out.println(i);
+            }
+        }
+    }
+}
