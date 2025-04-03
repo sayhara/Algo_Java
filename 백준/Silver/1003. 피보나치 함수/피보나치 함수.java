c683c9f8129033f9761406970ef409735930a8c5
sayhara <impact82@naver.com>
2025-04-03 20:36:15 +0900

    [Silver III] Title: 피보나치 함수, Time: 124 ms, Memory: 15960 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        int T=Integer.parseInt(br.readLine());
+
+        while(T-->0){
+            int N=Integer.parseInt(br.readLine());
+            int zero[]=new int[41];
+            int one[]=new int[41];
+            zero[0]=1;
+            zero[1]=0;
+            one[1]=1;
+            one[0]=0;
+            for(int i=2;i<=N;i++){
+                zero[i]=zero[i-1]+zero[i-2];
+                one[i]=one[i-1]+one[i-2];
+            }
+            System.out.println(zero[N]+" "+one[N]);
+        }
+    }
+}
