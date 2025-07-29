5222506fa21d87e20a5ba1bda9acd359437d99ad
sayhara <impact82@naver.com>
2025-07-29 21:44:54 +0900

    [Silver I] Title: IOIOI, Time: 236 ms, Memory: 20132 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -10,20 +10,26 @@ public class Main {
         int N=Integer.parseInt(br.readLine());
         int M=Integer.parseInt(br.readLine());
         String s=br.readLine();
+        int count=0;
+        int ans=0;
 
-        String chk="I";
-        for(int i=0;i<N;i++){
-            chk+="OI";
-        }
-
-        int cnt=0;
-        for(int i=0;i<=s.length()-chk.length();i++){
-            String s1=s.substring(i,i+chk.length());
-            if(s1.equals(chk)){
-                cnt++;
+        for(int i=1;i<M-1;){
+            if(s.charAt(i)=='O' && s.charAt(i+1)=='I'){
+                count++;
+                if(count==N){
+                    if(s.charAt(i+1-2*count)=='I'){
+                        ans++;
+                    }
+                    count--;
+                }
+                i+=2;
+            }
+            else{
+                count=0;
+                i++;
             }
         }
-        System.out.println(cnt);
 
+        System.out.println(ans);
     }
 }
